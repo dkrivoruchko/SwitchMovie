a5eca27978b7b9ecadd9739804e888cbeac815dc
Dmitriy Krivoruchko <dkrivoruchko@gmail.com>
2017-12-15 16:54:47 +0200

    Libs versions update

--- data/src/main/java/info/dvkr/switchmovie/data/utils/Preference.kt
+++ data/src/main/java/info/dvkr/switchmovie/data/utils/Preference.kt
@@ -18,17 +18,18 @@ fun <T> bindPreference(preferences: Preferences, key: String, defaultValue: T): 
       else -> throw IllegalArgumentException("Unsupported preference type")
     } as ReadWriteProperty<Any, T>
 
-private class PreferenceDelegate<T>(
-    private val preferences: Preferences,
-    private val key: String,
-    private val defaultValue: T,
-    private val getter: Preferences.(String, T) -> T,
-    private val setter: PreferencesEditor.(String, T) -> PreferencesEditor
-) : ReadWriteProperty<Any, T> {
+private class PreferenceDelegate<T>(private val preferences: Preferences,
+                                    private val key: String,
+                                    private val defaultValue: T,
+                                    private val getter: Preferences.(String, T) -> T,
+                                    private val setter: PreferencesEditor.(String, T) -> PreferencesEditor)
+  : ReadWriteProperty<Any, T> {
+
   override fun getValue(thisRef: Any, property: KProperty<*>) =
       preferences.getter(key, defaultValue)
 
   override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
     preferences.edit().setter(key, value).commit()
+
   }
 }
